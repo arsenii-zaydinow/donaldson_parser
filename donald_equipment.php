000422ac24d49b5f37491cfa46822b33f6f68464
Arsenii Zaydinow <zaydinow.seny@gmail.com>
2020-08-01 13:47:14 +0500

    Добавлена загрузка оборудования в базу данных MySQL

--- donald_equipment.php
+++ donald_equipment.php
@@ -32,7 +32,7 @@
 	$donaldLink = 'https://shop.donaldson.com/store/ru-ru/home';
 	$url = 'https://shop.donaldson.com/store/rest/fetchproductequipmentlist?id=';
 		
-	for ($r = 0; $r < 1; $r++) {
+	for ($r = 0; $r < count($details); $r++) {
 		
 		$filename = $dir.$details[$r];
 		$data = file_get_contents($filename);
@@ -42,7 +42,6 @@
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		//curl_setopt($ch, CURLOPT_HEADER, true);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers );
 		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36");
 		curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiefile);
@@ -56,8 +55,8 @@
 			$headers = [
 			'service_token: 3TuSgTm3MyS9ZL0adPDjYg=='
 			];
-			
-			for ($f = 51; $f < 52; $f++) {
+
+			for ($f = 0; $f < count($parts); $f++) {
 
 				$tableEqp = array();
 
@@ -67,7 +66,7 @@
 				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers );
 				curl_setopt($ch, CURLOPT_URL, $link);
 				$html = curl_exec($ch);
-				
+			
 				if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == "200") {
 					$json = json_decode($html, TRUE);
 					$parseEqp = $json['equipmentList'];
@@ -75,25 +74,21 @@
 					if (count($parseEqp) > 0) {
 						for($h = 0; $h < count($parseEqp); $h++){
 
-							$tableEqp[$h]["art"] = $parts[$f]["art"];
-							$tableEqp[$h]["producer"] = $parseEqp[$h]['equipmentMakeDisplayName']."<br>";
-							$tableEqp[$h]["eqpModel"] = $parseEqp[$h]['equipmentModel']."<br>";
-							$tableEqp[$h]["eqpType"] = $parseEqp[$h]['equipmentTypeDisplayName']."<br>";
-
 							if ($parseEqp[$h]['engineMakeDisplayName'] == '-') {
-								$tableEqp[$h]["engineModel"] = $parseEqp[$h]['equipmentEngineModel']."<br>";
+								$engineModel = $parseEqp[$h]['equipmentEngineModel'];
 							}
 							else {
-								$tableEqp[$h]["engineModel"] = $parseEqp[$h]['engineMakeDisplayName'].' '.$parseEqp[$h]['equipmentEngineModel']."<br>";
+								$engineModel = $parseEqp[$h]['engineMakeDisplayName'].' '.$parseEqp[$h]['equipmentEngineModel'];
 							}
-							
-							/*$sql = "INSERT INTO equipment (art, producer, eqpModel, eqpType, engineModel) VALUES ('$tableEqp[$h]['art']', '$tableEqp[$h]['producer']', '$tableEqp[$h]['eqpModel']', '$tableEqp[$h]['eqpType']', '$tableEqp[$h]['eqpType']')";
+
+							$sql = "INSERT INTO equipment (art, producer, eqpModel, eqpType, engineModel) VALUES ('".trim($parts[$f]['art'])."', '".$parseEqp[$h]['equipmentMakeDisplayName']."', '".$parseEqp[$h]['equipmentModel']."', '".$parseEqp[$h]['equipmentTypeDisplayName']."', '$engineModel')";
+
 							if (mysqli_query($conn, $sql)) {
-      							echo "New record created successfully";
+      							echo "New record created successfully <br>";
 							} else {
      							echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 							}
-							break;*/
+
 						}
 					}
 				}
@@ -101,20 +96,17 @@
 					echo 'Не удалось запросить оборудование со страницы '.$link;
 					break 2;
 				}
+
 			}
 		}
 		else {
 			echo 'Не удалось получить cookie со страницы '.$donaldLink;
 			break;
 		}
-		
+
 	}
 
 	curl_close ($ch);
 	mysqli_close($conn);
-
-	/*foreach ($tableEqp as $key => $value) {
-		echo $key." | ".$tableEqp[$key]["art"]." | ".$tableEqp[$key]["producer"]." | ".$tableEqp[$key]["eqpModel"]." | ".$tableEqp[$key]["eqpType"]." | ".$tableEqp[$key]["engineModel"]."<br>";
-	}*/
 	
 ?>
